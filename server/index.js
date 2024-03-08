62c2e6822e1ecdfb34eeb50ed64bbadebd7c4eb0
wedens-elma <wedens.elma@ui.ac.id>
2024-03-08 20:59:23 +0700

    server: attempt to make server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,49 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  let filePath = "." + req.url;
+
+  if (filePath === "./") {
+    filePath = "./scripts/index.html";
+  }
+
+  if (filePath === "./cars") {
+    filePath = "./scripts/cars.html";
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const contentType =
+    {
+      ".html": "text/html",
+      ".css": "text/css",
+      ".js": "text/javascript",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+    }[extname] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404);
+        res.end("404 Not Found");
+      } else {
+        res.writeHead(500);
+        res.end("500 Internal Server Error");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
